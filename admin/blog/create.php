c30f56248db1e31de09179f9c70eb670165175b7
jiguvagadiya <81074174+jiguvagadiya@users.noreply.github.com>
2023-02-06 10:51:59 +0530

    admin penal create and index page

--- admin/blog/create.php
+++ admin/blog/create.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>Crypto Blog</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<?php
+    <title>Crypto Blog</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <?php
 include(__DIR__ . './../includes/header.php');
 ?>
 
-<?php
+    <?php
         if (isset($_POST['submit'])) {
 
 
@@ -45,35 +47,39 @@ include(__DIR__ . './../includes/header.php');
 
         ?>
 </head>
+
 <body>
-<div class="app-content content ">
-    <div class="content-wrapper container-xxl p-0 pt-3">
-        <div class="content-body">
-            <section class="bs-validation">
-                <div class="row">
-                    <!-- Bootstrap Validation -->
-                    <div class="col-md-8 col-12">
-                        <div class="card">
-                            <div class="card-header pb-0">
-                                <h4 class="card-title">Create blog</h4>
-                                <div class=" float-end">
-                                <!-- <button name="submit" type="submit" value="submit" class="btn btn-primary">Submit</botton></br> -->
+    <div class="app-content content ">
+        <div class="content-wrapper container-xxl p-0 pt-5">
+            <div class="content-body">
+                <section class="bs-validation">
+                    <div class="row">
+                        <!-- Bootstrap Validation -->
+                        <div class="col-md-8 col-12">
+                            <div class="card">
+                                <div class="card-header pb-0">
+                                    <h5 class="card-title">Create blog</h5>
+                                    <div class=" float-end">
+                                        <!-- <button name="submit" type="submit" value="submit" class="btn btn-primary">Submit</botton></br> -->
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="card-body pt-0">
-                                <form name="blog" id="blog" class="validate-form1 needs-validation"
-                                    action="" method="POST"
-                                    enctype="multipart/form-data" novalidate>
-                                     <!--Start Form Field  -->
-                                    <div class="input-form mb-1">
-                                            <label class="form-label" for="title">title<span  class="text-danger">*</span></label>
-                                            <input type="text" id="title" name="title" class="form-control " value="" placeholder="Enter Your title" autofocus required /> 
-                                    </div> 
-                                    <div class="input-form mb-1">
-                                            <label class="form-label" for="category_id">category_id<span  class="text-danger">*</span></label>
+                                <div class="card-body pt-0">
+                                    <form name="blog" id="blog" class="validate-form1 needs-validation" action=""
+                                        method="POST" enctype="multipart/form-data" novalidate>
+                                        <!--Start Form Field  -->
+                                        <div class="input-form my-2">
+                                            <label class="form-label" for="title">title<span
+                                                    class="text-danger">*</span></label>
+                                            <input type="text" id="title" name="title" class="form-control " value=""
+                                                placeholder="Enter Your title" autofocus required />
+                                        </div>
+                                        <div class="input-form my-2">
+                                            <label class="form-label" for="category_id">category_id<span
+                                                    class="text-danger">*</span></label>
                                             <!-- <input type="text" id="category_id" category_id="category_id" class="form-control " value="" placeholder="Enter Your category_id" autofocus required />  -->
-                                            <select name="category_id" id="category_id" class="form-select form-control">
-                                            <?php
+                                            <select name="category_id" id="category_id"
+                                                class="form-select form-control">
+                                                <?php
                                                 $records = mysqli_query($conn, "SELECT category_name,id  From blogcategory");  // Use select query here
                                                 while ($data = mysqli_fetch_array($records))
                                                 {
@@ -84,34 +90,47 @@ include(__DIR__ . './../includes/header.php');
                                                 <option value="2">2</option>
                                                 <option value="3">3</option> -->
                                             </select>
-                                    </div>  
-                                    <div class="input-form mb-1">
-                                            <label class="form-label" for="image">Image<span  class="text-danger">*</span></label>
-                                            <input type="file" id="image" name="image" class="form-control " value="" placeholder="Enter Your image" autofocus required /> 
-                                    </div>  
-                                    <div class="input-form mb-1">
-                                            <label class="form-label" for="description">Description<span  class="text-danger">*</span></label>
-                                            <input type="text" id="description" name="description" class="form-control " value="" placeholder="Enter Your Description" autofocus required /> 
-                                    </div> 
-                                     <!--End Form Field  -->
-                                     <hr>
-                                    <div class="input-form mt-1">
-                                    <button name="submit" type="submit" value="submit" class="btn btn-primary">Submit</botton></br>
-                                    </div>        
-                                </form>
+                                        </div>
+                                        <div class="input-form my-2">
+                                            <label class="form-label" for="image">Image<span
+                                                    class="text-danger">*</span></label>
+                                            <input type="file" id="image" name="image" class="form-control " value=""
+                                                placeholder="Enter Your image" autofocus required />
+                                        </div>
+                                        <div class="input-form my-2">
+                                            <label class="form-label" for="description">Description<span
+                                                    class="text-danger">*</span></label>
+                                            <input type="text" id="description" name="description" class="form-control "
+                                                value="" placeholder="Enter Your Description" autofocus required />
+                                        </div>
+                                        <!--End Form Field  -->
+                                        <hr>
+                                        <div class="input-form my-1">
+                                            <button type="submit" name="submit" value="submit"
+                                                class="btn btn-success"><b>Save</b></button>
+                                            <button type="reset" name="reset" value="reset"
+                                                class="btn btn-secondary"><b>Reset</b></button>
+                                            <a href="./index.php" class="btn btn-dark float-end"><b>Back</b></a>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
+                        <!-- /Bootstrap Validation -->
                     </div>
-                    <!-- /Bootstrap Validation -->
-                </div>
-            </section>
+                </section>
+            </div>
         </div>
     </div>
-</div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
-
-
--- admin/blog/index.php
+++ admin/blog/index.php
@@ -1,44 +1,54 @@
 <?php
 include(__DIR__ . './../includes/header.php');
 ?>
-    <div class="content-wrapper container-xxl p-0 pt-3">
-        <div class="card mb-4">
-            <div class="card-header bg-light"><i class="fas fa-table me-1"></i> Blog Table</div>
-                <div class="card-body">
-                <h2><a href="./create.php" class="btn btn-primary"><i class="fa fa-plus me-1"></i><B>Create</b></a></h2>
-                <table id="datatablesSimple">
-                    <thead>
-                        <tr>
+<div class="content-wrapper container-xxl p-0 pt-5">
+    <div class="card mb-4 d-flex">
+        <div class="card-header d-flex justify-content-between align-items-center">
+            <h5>List of Blog</h5>
+            <div class="col-sm-2 align-items-start justify-content-end me-2">
+                <a href="./create.php" class="btn btn-dark float-end"><i class="fa fa-plus me-1"></i><B>Create</b></a>
+            </div>
+        </div>
+        <div class="card-body">
+            <table id="datatablesSimple">
+                <thead>
+                    <tr>
                         <th>id</th>
                         <th>Image</th>
                         <th>Title</th>
                         <th>Category</th>
                         <th>Description</th>
                         <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <?php
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <?php
                                 $sql = "SELECT * from blog";
                                 $query = mysqli_query($conn, $sql);
                                 while ($row = mysqli_fetch_assoc($query)) {
                             ?>
-                            <td><?php echo $row['id']; ?></td>
-                            <td> <img src="<?php echo $row["image"]; ?>" height=100px; width="100px;"></td>
-                            <td><?php echo $row['title']; ?></td>
-                            <td><?php echo $row['category_id']; ?></td>
-                            <td><?php echo $row['description']; ?></td>
-                            <td><a href="./update.php?id=<?php echo $row['id']; ?>"><button class="btn btn-success"><i class=""></i>edit</button></a>
-                                <a href="./delete.php?id=<?php echo $row['id']; ?>"><button class="btn btn-danger"><i class=""></i>delete</button></a>
-                            </td>
-                        </tr>
-                        <?php } ?>
-                    </tbody>
-                </table>
-            </div>
+                        <td><?php echo $row['id']; ?></td>
+                        <td> <img src="<?php echo $row["image"]; ?>" height=100px; width="100px;"></td>
+                        <td><?php echo $row['title']; ?></td>
+                        <td><?php echo $row['category_id']; ?></td>
+                        <td><?php echo $row['description']; ?></td>
+                        <td>
+
+                            <a href="./update.php?id=<?php echo $row['id']; ?>"><button class="btn btn-success"><i
+                                        class="fa-solid fa-pen-to-square"></i></button></a>
+                            <a href="./delete.php?id=<?php echo $row['id']; ?>"><button class="btn btn-danger"><i
+                                        class="fa-solid fa-trash"></i></button></a>
+                            <a href="./delete.php?id=<?php echo $row['id']; ?>"><button class="btn btn-warning">
+                                    <i class="fa-solid fa-eye"></i></button></a>
+                        </td>
+                    </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
         </div>
     </div>
+</div>
 <?php
 include(__DIR__ . './../includes/footer.php');
 include(__DIR__ . './../includes/script.php');
