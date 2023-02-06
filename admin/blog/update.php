9b2ada32c027bdbd98ce626034504af64e11748a
jiguvagadiya <81074174+jiguvagadiya@users.noreply.github.com>
2023-02-06 12:52:47 +0530

    update design

--- admin/blog/update.php
+++ admin/blog/update.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>Crypto Blog</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<?php
+    <title>Crypto Blog</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <?php
 include(__DIR__ . './../includes/header.php');
 ?>
 
-<?php
+    <?php
       
 
         if (isset($_POST['submit'])) {
@@ -50,8 +52,8 @@ include(__DIR__ . './../includes/header.php');
                 include('./index.php');
             }
         } else { ?>
-        
-        <?php
+
+    <?php
          include(__DIR__ . './../database.php');
 
             $id = $_GET['id'];
@@ -67,75 +69,99 @@ include(__DIR__ . './../includes/header.php');
             while ($row = mysqli_fetch_assoc($query)) {
     ?>
 
-<div class="app-content content ">
-    <div class="content-wrapper container-xxl p-0 pt-2">
-        <div class="content-body">
-            <section class="bs-validation">
-                <div class="row">
-                    <!-- Bootstrap Validation -->
-                    <div class="col-md-8 col-12">
-                        <div class="card">
-                            <div class="card-header pb-0">
-                                <h4 class="card-title">Create blog</h4>
-                                <div class=" float-end">
-                                <!-- <button name="submit" type="submit" value="submit" class="btn btn-primary">Submit</botton></br> -->
+    <div class="app-content content ">
+        <div class="content-wrapper container-xxl p-0 pt-5">
+            <div class="content-body">
+                <section class="bs-validation">
+                    <div class="row">
+                        <!-- Bootstrap Validation -->
+                        <div class="col-md-8 col-12">
+                            <div class="card">
+                                <div class="card-header pb-0">
+                                    <h5 class="card-title">Edit blog</h5>
+                                    <div class=" float-end">
+                                        <!-- <button name="submit" type="submit" value="submit" class="btn btn-primary">Submit</botton></br> -->
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="card-body pt-0">
-                                <form name="blog" id="blog" class="validate-form1 needs-validation"
-                                    action="" method="POST"
-                                    enctype="multipart/form-data" novalidate>
+                                <div class="card-body pt-0">
+                                    <form name="blog" id="blog" class="validate-form1 needs-validation" action=""
+                                        method="POST" enctype="multipart/form-data" novalidate>
 
-                                     <!--Start Form Field  -->
-                                     <input type="text" id="id" name="id" placeholder="id..." value="<?php echo $row['id']; ?>" readonly>
+                                        <!--Start Form Field  -->
+                                        <!-- <input type="text" id="id" name="id" placeholder="id..." value="<?php echo $row['id']; ?>" readonly> -->
 
-                                    <div class="input-form mb-1">
-                                            <label class="form-label" for="title">title<span  class="text-danger">*</span></label>
-                                            <input type="text" id="title" name="title" class="form-control " value="<?php echo $row['title']; ?>" placeholder="Enter Your Name" autofocus required /> 
-                                    </div> 
-                                    <div class="input-form mb-1">
-                                            <label class="form-label" for="category_id">category_id<span  class="text-danger">*</span></label>
-                                            <input type="text" id="category_id" name="category_id" class="form-control " value="<?php echo $row['category_id']; ?>" placeholder="Enter Your Name" autofocus required /> 
-                                    </div>  
-                                    <div class="input-form mb-1">
-                                            <label class="form-label" for="image">Image<span  class="text-danger">*</span></label>
-                                            <input type="file" id="image" name="image" class="form-control " value="<?php echo $row['image']; ?>" placeholder="Enter Your image" autofocus required /> 
-                                    </div>  
-                                    <div class="input-form mb-1">
-                                            <label class="form-label" for="description">Description<span  class="text-danger">*</span></label>
-                                            <input type="text" id="description" name="description" class="form-control " value="<?php echo $row['description']; ?>" placeholder="Enter Your Description" autofocus required /> 
-                                    </div> 
+                                        <div class="input-form my-2">
+                                            <label class="form-label" for="title">title<span
+                                                    class="text-danger">*</span></label>
+                                            <input type="text" id="title" name="title" class="form-control "
+                                                value="<?php echo $row['title']; ?>" placeholder="Enter Your Name"
+                                                autofocus required />
+                                        </div>
+                                        <div class="input-form my-2">
+                                            <label class="form-label" for="category_id">category_id<span
+                                                    class="text-danger">*</span></label>
+                                            <input type="text" id="category_id" name="category_id" class="form-control "
+                                                value="<?php echo $row['category_id']; ?>" placeholder="Enter Your Name"
+                                                autofocus required />
+                                        </div>
+                                        <div class="input-form my-2">
+                                            <label class="form-label" for="image">Image<span
+                                                    class="text-danger">*</span></label>
+                                            <input type="file" id="image" name="image" class="form-control "
+                                                value="<?php echo $row['image']; ?>" placeholder="Enter Your image"
+                                                autofocus required />
+                                        </div>
+                                        <div class="input-form my-2">
+                                            <label class="form-label" for="description">Description<span
+                                                    class="text-danger">*</span></label>
+                                            <input type="text" id="description" name="description" class="form-control "
+                                                value="<?php echo $row['description']; ?>"
+                                                placeholder="Enter Your Description" autofocus required />
+                                        </div>
 
-                                    <div class='input-form mb-1'>
-                                            <label class='form-label'
-                                                for='fp-default'>status(dactive/active)</label>
+                                        <div class='input-form my-2'>
+                                            <label class='form-label' for='fp-default'>Status(dactive/active)</label>
                                             <div class='form-check form-switch form-check-primary'>
                                                 <input type='checkbox' id='status' value='1' name='status'
                                                     class='form-check-input' checked>
                                                 <label class='form-check-label' for='status'></label>
                                             </div>
                                         </div>
-                                     <!--End Form Field  -->
-                                     <hr>
-                                    <div class="input-form mt-1">
-                                    <button name="submit" type="submit" value="submit" class="btn btn-primary">Update</botton></br>
-                                    </div>  
-                                    <?php } ?>      
-                                </form>
+                                        <!--End Form Field  -->
+                                        <hr>
+                                     
+                                        <div class="input-form my-1">
+                                            <button type="submit" name="submit" value="submit"
+                                                class="btn btn-success"><b>Save</b></button>
+                                            <button type="reset" name="reset" value="reset"
+                                                class="btn btn-secondary"><b>Reset</b></button>
+                                            <a href="./delete.php?id=<?php echo $row['id']; ?>"><button
+                                                    class="btn btn-danger">
+                                                    <b>delete</b></button></a>
+                                            <a href="./index.php" class="btn btn-dark float-end"><b>Back</b></a>
+                                        </div>
+
+                                        <?php } ?>
+                                    </form>
+                                </div>
                             </div>
                         </div>
+                        <!-- /Bootstrap Validation -->
                     </div>
-                    <!-- /Bootstrap Validation -->
-                </div>
-            </section>
+                </section>
+            </div>
         </div>
     </div>
-</div>
-<?php } ?>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <?php } ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
-
-
