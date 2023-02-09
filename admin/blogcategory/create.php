97cf7b79ca540df6f46db88c7258e30ef2978b13
Jinal <81014173+jinalsuhagiya@users.noreply.github.com>
2023-02-09 14:15:21 +0530

    link update

--- admin/blogcategory/create.php
+++ admin/blogcategory/create.php
@@ -3,8 +3,6 @@
 
 <head>
     <title>Crypto blog Category</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <?php
     include(__DIR__ . './../includes/header.php');
 ?>
@@ -52,8 +50,9 @@
                                             <label class="form-label" for="category_name">Blog Category<span
                                                     class="text-danger">*</span></label>
                                             <input type="text" id="category_name" name="category_name"
-                                                class="form-control " value="" placeholder="Enter Your Category Name"
+                                                class="form-control " value="<?php echo $_POST['category_name']; ?>" placeholder="Enter Your Category Name"
                                                 autofocus required />
+                                                
                                         </div>
                                         <div class="input-form my-2">
                                             <label class="form-label" for="show_in_nav">Show Navigation<span
@@ -82,15 +81,6 @@
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
-        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
-        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
-    </script>
     <script>
         ClassicEditor
             .create( document.querySelector( '#editor' ) )
